251cabb61fbd9a31ea9889b3f30ce443b16dccac
zrma <bulbitain@gmail.com>
2019-10-27 22:08:01 +0900

    Add simple echo command

--- cmd/mud/main.go
+++ cmd/mud/main.go
@@ -39,28 +39,41 @@ func Register(word string, f func() (op, error)) error {
 	return nil
 }
 
-var _ = Register("끝", func() (op, error) {
+var _ = Register("끝", func() (o op, e error) {
 	return Exit, nil
 })
 
+var _ = Register("말", func() (o op, e error) {
+	return Echo, nil
+})
+
+func echo(args ...string) {
+	const whitespace = " "
+	fmt.Println("당신:", strings.Join(args, whitespace))
+}
+
 func main() {
 	const (
-		delimiterRune = '\n'
-		delimiterStr  = string(delimiterRune)
+		linefeed    = '\n'
+		linefeedStr = string(linefeed)
+		whitespace  = " "
 	)
 
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("] ")
-		text, err := reader.ReadString(delimiterRune)
+		input, err := reader.ReadString(linefeed)
 		if err != nil {
 			log.Println(err)
 		}
 
-		text = strings.TrimRight(text, delimiterStr)
-		cmd, ok := commands[text]
+		input = strings.TrimRight(input, linefeedStr)
+		inputs := strings.Split(input, whitespace)
+
+		args, token := inputs[:len(inputs)-1], inputs[len(inputs)-1]
+		cmd, ok := commands[token]
 		if !ok {
-			fmt.Println("그런 명령어는 찾을 수 없습니다:", text)
+			fmt.Println("그런 명령어는 찾을 수 없습니다:", input)
 			continue
 		}
 
@@ -73,7 +86,9 @@ func main() {
 		case Exit:
 			fmt.Println("접속을 종료합니다.")
 			return
+		case Echo:
+			echo(args...)
 		}
-		fmt.Println(text)
+		fmt.Println(input)
 	}
 }
